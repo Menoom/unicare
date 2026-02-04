63890d97dec0e533ebb9f84c08e7a4bd8fccc4ba
Menoom <dishatripathy22@gmail.com>
2026-02-04 22:12:45 +0530

    Created doctor credential tables

---
+++ database/migrations/2026_02_04_162041_create_doctors_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('doctors', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete()->unique();
+            $table->string('specialization');
+            $table->unsignedInteger('experience_years');
+            $table->decimal('consultation_fee', 6, 2)->unsigned() ;
+
+            $table->enum('job_type', ['private', 'hospital_based']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('doctors');
+    }
+};
